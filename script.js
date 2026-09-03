b82a3701b72a85fddf1d6624299bf31d7ebd3426
andois <andonisspoli10@gmail.com>
2026-09-03 12:41:27 +0300

    added new files

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let name='andonis';
 
-//let butten=document.getElementById('start');
-//butten.addEventListener('click',function(){
-//    alert(name);
-//}
-//);
+let butten=document.getElementById('start');
+butten.addEventListener('click',function(){
+    window.location.href='play.href';
+}
+);
+
 function opensetings(){
     window.location.href='settings.html';
 };
+//function gameplay(){
+//    window.location.href='play.html';
+//};
