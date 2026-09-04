c0d0a0fcc058d04ce37baf6df36a5cb42ca5ec89
andois <andonisspoli10@gmail.com>
2026-09-04 19:43:27 +0300

    started making gameplay

--- gamae.py
+++ gamae.py
@@ -1,13 +1,39 @@
 import pygame
+import time
 pygame.init()
 screen=pygame.display.set_mode((800,600))
 clock=pygame.time.Clock()
-player=pygame.Rect(100,400,50,50)
+player1=pygame.Rect(0,0,50,50)
 runing=True
+floor=pygame.Rect(0,590,800,10)
+player1_vl_x=0
+player1_vl_y=0
+
 while runing:
-    screen.fill('blue')
-    pygame.draw.rect(screen,'red',player)
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            runing=False
+    if player1_vl_y<5:
+        player1_vl_y+=1 
+    player1.y+=player1_vl_y
+    key=pygame.key.get_pressed()
+
+    if key[pygame.K_d]:
+        player1.x+=1
+    elif key[pygame.K_a]:
+        player1.x-=1
+
+    if player1.colliderect(floor):
+            player1.y=floor.y-player1.height
+            player1_vl_y=0
+        
+    if key[pygame.K_SPACE]:
+        player1_vl_y=-2
+
+    screen.fill('purple')
+    pygame.draw.rect(screen,'red',player1)
+    pygame.draw.rect(screen,'brown',floor)
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(150)
 
 pygame.quit()
